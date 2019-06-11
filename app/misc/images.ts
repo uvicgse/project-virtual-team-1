5891c5bd9f5c14846386759046cf7a4d40669862
Norris Ng <6787487+norrisng@users.noreply.github.com>
2019-06-10 18:09:43 -0700

    Partially implement GitHub profile pic retrieval, refactor existing code

--- app/misc/images.ts
+++ app/misc/images.ts
@@ -3,31 +3,67 @@ let images = {};
 let imageFiles = ["jarjar.jpg", "yoda.png", "obiwan.jpg"];
 let imageCount = 0;
 let githubAvatarUrl = require('github-avatar-url');
+let gh = require('octonode');
+
+/**
+ * Helper function for getting the letter icon for a given author.
+ * 
+ * @param name Name of the commit's contributor
+ * @returns filepath for the given person's first initial
+ */
+function getLetterIcon(name: string) {
+  let first = name.trim().charAt(0).toUpperCase();
+  return "node_modules/material-letter-icons/dist/png/" + first + ".png";
+}
 
 function getName(author: string) {
   let name = author.split("<")[0];
   return name;
 }
 
-function img4User(name:string) {
-  let pic;
-  let first = name.trim().charAt(0).toUpperCase();
-  pic =  "node_modules/material-letter-icons/dist/png/" + first + ".png";
-  return pic;
+function img4User(name:string) {  
+  return getLetterIcon(name);
 }
 
+// seems like this function was implemented as scaffolding for the profile pic feature.
 function imageForUser(name: string, email: string, callback) {
-  let pic;
-  githubAvatarUrl(name, function(err,avatarURL){
-    if(!err) {
-      console.log("avatar url: avatarURL");
-      pic = avatarURL;
-    } else {
-      let first = name.trim().charAt(0).toUpperCase();
-      pic =  "node_modules/material-letter-icons/dist/png/" + first + ".png";
-    }
-    callback(pic);
-  });
+  
+  let pic;  
+  
+  if (email.includes('@users.noreply.github.com')) {
+    
+    // Extract GitHub username from noreply email (<number>+<username>@users.noreply.github.com)
+    // This won't work for users that are NOT hiding their email in the GitHub settings.
+    // See https://help.github.com/en/articles/about-commit-email-addresses for more info.
+    let username = email.replace('@users.noreply.github.com','').replace(/^(\d){7}\+/,'');
+
+    // github-avatar-url insists on having an API token, 
+    // but since we're don't need it for public info, we'll instead use octonode to avoid having said token.
+    let client = gh.client();
+    client.get(`/users/${username}`, {}, function (err, status, body, headers) {
+      if (!err)
+        pic = body.avatar_url;
+      else
+        pic = getLetterIcon(name);
+    });
+  }
+  // fallback to letter icons if the email isn't a GitHub noreply one
+  else {
+    pic = getLetterIcon(name);
+  }  
+  callback(pic);
+  // githubAvatarUrl(username, function(err,avatarURL){
+  //   if(!err && !email.includes('@users.noreply.github.com')) {
+  //     console.log(`avatar url: ${avatarURL}`);
+  //     pic = avatarURL;
+  //   } 
+  //   // fallback to letter icons if the email isn't a GitHub noreply one
+  //   else {
+  //     let first = name.trim().charAt(0).toUpperCase();
+  //     pic =  "node_modules/material-letter-icons/dist/png/" + first + ".png";
+  //   }
+    
+  // });
 
   // if (images[email] === undefined) {
   //   images[email] = "assets/img/starwars/" + imageFiles[imageCount];
