701c40dbe4e70c8272e2f74f020be1a530e1d7af
Sunil Kumar <sunilkumarsheoran@gmail.com>
2019-06-12 16:19:34 -0700

    relocated the error

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,3 @@ import { bootstrap }    from "@angular/platform-browser-dynamic";
 import { AppComponent } from "./components/app/app.component";
 import { AuthenticateComponent} from "./components/authenticate/authenticate.component";
 bootstrap(AppComponent);
-
-//creating dummy error so that tsc warning wont show up
-generateDummyError(
